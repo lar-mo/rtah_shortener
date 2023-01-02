b9315d4f00aa2c9b0f3c55057269a4b9de347fe0
Larry Moiola <lmoiola@gmail.com>
2023-01-02 14:29:38 -0800

    handling url GET args custom code

--- rtah_utils/url_shortener/views.py
+++ rtah_utils/url_shortener/views.py
@@ -32,7 +32,7 @@ def saveurl(request):
                 for i in range(5):
                     shorturl_code += random.choice(letters_digits)
                 exist_count = shortened_url.objects.filter(code=shorturl_code).count()
-                if exist_count >= 1:
+                if exist_count > 0:
                     print("shorturl_code already exists")
                 else:
                     short_url = shortened_url(code=shorturl_code, long_url=long_url)
@@ -43,8 +43,12 @@ def saveurl(request):
     return HttpResponseRedirect(reverse('url_shortener:index'))
 
 def redir_to_long_url(request, code):
+    year = request.GET.get('year', '')
     try:
         url_object = shortened_url.objects.get(code=code)
+        if code == 'h':
+            if year:
+                return redirect(url_object.long_url + "?" + year + "&" + "full")
         return redirect(url_object.long_url)
     except ObjectDoesNotExist:
         return HttpResponse("The code does not exist!")
